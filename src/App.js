c14a7441246e9c03a2416af958701acff12f0224
Adam Hermansson <qurridutt@gmail.com>
2019-06-05 21:30:56 +0200

    Cat facts data fetching is now complete! Saves data to state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    catFact: ""
+  };
+
+  componentDidMount() {
+    this.getRandomCatFact();
+  }
+
+  getRandomCatFact() {
+    axios.get("https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random").then(response => {
+      this.setState({
+        catFact: response.data.text
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p> {this.state.catFact} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
