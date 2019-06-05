07ea2f68ba2d55bc5e4186992f8f46fdced8b942
Adam Hermansson <qurridutt@gmail.com>
2019-06-06 00:19:45 +0200

    Animation implemented! And also a full refactorization... new architecture with hooks instead of state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from 'react'
+import { useTransition, animated } from 'react-spring'
 import axios from "axios";
+import './App.css'
 
-class App extends Component {
-  state = {
-    catFact: ""
-  };
 
-  componentDidMount() {
-    this.getRandomCatFact();
-  }
+export default function App() {
 
-  getRandomCatFact = () => {
-    axios.get("https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random").then(response => {
-      this.setState({
-        catFact: response.data.text
-      })
-    });
-  }
+  const [index, setIndex] = useState(0);
+  const [catFact, setCatFact] = useState('');
 
-  render() {
-    return (
-      <div>
-        <p> {this.state.catFact} </p>
-        <button onClick={this.getRandomCatFact}> Meow! </button>
+  const pages = [
+    ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>{catFact}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>{catFact}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>{catFact}</animated.div>,
+  ]
+
+ function getRandomCatFact() {
+  axios.get('https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random').then(response => {
+    const newCatFact = response.data.text;
+    setCatFact(newCatFact);
+    setIndex(index => (index + 1) % 3);
+  })
+};
+  
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+
+  return (
+      <div className="colorfulMain" onClick={getRandomCatFact}>
+        {transitions.map(({ item, props, key }) => {
+          const Page = pages[item]
+          return <Page key={key} style={props} />
+        })}
       </div>
-    );
-  }
+  )
 }
-
-export default App;
