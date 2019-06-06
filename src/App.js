af5b8ed4d2015004eda4c8664a5d766cd95a59da
Adam Hermansson <qurridutt@gmail.com>
2019-06-06 19:22:46 +0200

    Landing screen implemented

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ export default function App() {
   const [index, setIndex] = useState(0);
   const [catFact, setCatFact] = useState('');
 
+  const catEmoji = <span role="img" aria-label="Cat emoji">😺</span>;
+
   const pages = [
-    ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>{catFact}</animated.div>,
-    ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>{catFact}</animated.div>,
-    ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>{catFact}</animated.div>,
-  ]
+    ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightcoral' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightcyan' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightsalmon' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+    ({ style }) => <animated.div style={{ ...style, background: 'lightseagreen' }}>{catEmoji}{catFact}{catEmoji}</animated.div>,
+  ];
+
+  const LandingPage = ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>{catEmoji}Press anywhere on the screen to generate a cat fact!{catEmoji}</animated.div>;
 
  function getRandomCatFact() {
   axios.get('https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random').then(response => {
@@ -32,8 +40,12 @@ export default function App() {
   return (
       <div className="colorfulMain" onClick={getRandomCatFact}>
         {transitions.map(({ item, props, key }) => {
-          const Page = pages[item]
-          return <Page key={key} style={props} />
+          const Page = pages[item];
+          if (catFact === '') {
+            return <LandingPage />
+          } else {
+            return <Page key={key} style={props} />
+          }
         })}
       </div>
   )
